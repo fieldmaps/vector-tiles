02c9552e59d691f460891e6d99287312513500a2
Max Malynowsky <github@maxmalynowsky.com>
2024-07-29 09:12:25 +0200

    update planet sync to guarentee data availability

--- sync_planet.py
+++ sync_planet.py
@@ -1,19 +1,21 @@
-import subprocess
-from datetime import date
+from datetime import date, timedelta
 from pathlib import Path
+from subprocess import run
 
 cwd = Path(__file__).parent
 
 if __name__ == "__main__":
-    today = date.today().strftime("%Y%m%d")
-    subprocess.run(
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    yesterday_str = yesterday.strftime("%Y%m%d")
+    run(
         [
             "rclone",
             "copyurl",
             "--progress",
             "--s3-no-check-bucket",
             "--s3-chunk-size=256M",
-            f"https://build.protomaps.com/{today}.pmtiles",
+            f"https://build.protomaps.com/{yesterday_str}.pmtiles",
             "r2://fieldmaps-tiles/planet.pmtiles",
         ]
     )
